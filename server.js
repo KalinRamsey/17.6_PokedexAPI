2c11e18d800a7bbf3509fecc2459a36bac0ca0a0
Kalin Ramsey <kalinramsey@gmail.com>
2020-05-27 17:40:22 -0700

    added API token to .env, trying to run debugger to validate authentication

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
+console.log(process.env.API_TOKEN);
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
-app.use((req, res) => {
-  res.send('Hello, moon!');
+const app = express();
+app.use(morgan('dev'));
+
+app.use(function validateBearerToken(req, res, next) {
+  console.log('validate bearer token middleware')
+  debugger
+  // move to the next middleware
+  next()
 })
+
+function handleGetTypes(req, res) {
+  res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+  res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000
 
